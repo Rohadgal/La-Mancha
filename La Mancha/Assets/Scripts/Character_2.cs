4279a465e35681b2fbb86fea8f4120de16d455c5
Gal <gal.frias@gmail.com>
2022-09-06 23:07:51 -0500

    HUD vidas

--- La Mancha/Assets/Scripts/Character_2.cs
+++ La Mancha/Assets/Scripts/Character_2.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character_2 : MonoBehaviour
 {
+    public static Image image;
+    [SerializeField]
+    Image s_image;
     public float life;
+
+    private void Start()
+    {
+        image = s_image;
+        image.fillAmount = 1;
+    }
+    public static void TakeDamage()
+    {
+        image.fillAmount -= .333f;
+        if(image.fillAmount < .2f)
+        {
+            image.fillAmount = 0;
+        }
+    }
 }
