92b6fa4dcd476205976e39edd0ad73e1ba07ea13
Gal <gal.frias@gmail.com>
2022-09-07 22:52:13 -0500

    HUD - keys and Mancha

--- La Mancha/Assets/Scripts/Character_2.cs
+++ La Mancha/Assets/Scripts/Character_2.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Character_2 : MonoBehaviour
 {
     public static Image image;
+    public TMP_Text textKeys;
+    public TMP_Text textManchas;
     [SerializeField]
     Image s_image;
     public float life;
+    public int keys;
+    public int mancha;
 
     private void Start()
     {
         image = s_image;
         image.fillAmount = 1;
+
+        textKeys.text = "x " + keys.ToString();
+        textManchas.text = "x " + mancha.ToString();
     }
     public static void TakeDamage()
     {
